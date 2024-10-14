ff1fa44304d905dd9da64688d5bdf89084320a0a
Sefinek <contact@sefinek.net>
2024-10-14 06:01:04 +0200

    Content updated, date: 14.10.2024, 06:01:04

--- services/axios.js
+++ services/axios.js
@@ -1,14 +1,20 @@
 const axios = require('axios');
+const { name, version, homepage } = require('../package.json');
 
-const instance = axios.create({
-	headers: {
-		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-		'Accept-Language': 'pl;q=0.7',
-		'Accept-Encoding': 'gzip, deflate, br, zstd',
-		'Connection': 'keep-alive',
-		'Upgrade-Insecure-Requests': '1'
-	}
-});
+axios.defaults.headers.common = {
+	'User-Agent': `Mozilla/5.0 (compatible; ${name}/${version}; +${homepage})`,
+	'Accept': 'application/json;v=b3;q=0.7',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'en-US,en;q=0.9',
+	'Cache-Control': 'max-age=0',
+	'Connection': 'keep-alive',
+	'Upgrade-Insecure-Requests': '1',
+	'Sec-Fetch-Dest': 'document',
+	'Sec-Fetch-Mode': 'navigate',
+	'Sec-Fetch-Site': 'same-origin',
+	'Sec-Fetch-User': '?1',
+	'Sec-CH-UA-Mobile': '?0',
+	'Priority': 'u=0, i'
+};
 
-module.exports = instance;
+module.exports = axios;
